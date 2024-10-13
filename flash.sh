828e8edc78059c9a983e3347c6f7e5203db4c99e
Ritik Garg <83335217+ritikgarg829@users.noreply.github.com>
2024-10-13 19:58:00 +0530

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/flash/flash.py",
+    "postStartCommand": "python3 /workspaces/BGMI/flash.py",
     "customizations": {
         "vscode": {
             "settings": {
